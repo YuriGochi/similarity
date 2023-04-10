f2415a96c20c87470331bdf64f0832744254e099
Yuri Oliveira Gochi <yurigochi_otg@cnc.org.br>
2023-04-10 15:21:42 -0300

    component paragraph, heading and alias in import, fix the styles/globals.css

---
+++ src/app/componentes/ui/LargeHeading.tsx
@@ -0,0 +1,40 @@
+import React, { HTMLAttributes } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/app/lib/utils";
+
+const HeadingVariants = cva(
+  "text-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter",
+  {
+    variants: {
+      size: {
+        default: "text-4x1 md:text-5xl lg:text-6xl",
+        lg: "text-5x1 md:text-6xl lg:text-7xl",
+        sm: "text-2x1 md:text-3xl lg:text-4xl",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface LargeHeadgingProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof HeadingVariants> {}
+
+const Paragraph = React.forwardRef<HTMLHeadingElement, LargeHeadgingProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <h1
+        ref={ref}
+        {...props}
+        className={cn(HeadingVariants({ size, className }))}
+      >
+        {children}
+      </h1>
+    );
+  }
+);
+
+Paragraph.displayName = "Paragraph";
+export default Paragraph;
---
+++ src/app/componentes/ui/Paragraph.tsx
@@ -0,0 +1,39 @@
+import React, { HTMLAttributes } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/app/lib/utils";
+
+const ParagraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm, sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof ParagraphVariants> {}
+
+const Paragraph = React.forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <p
+        ref={ref}
+        {...props}
+        className={cn(ParagraphVariants({ size, className }))}
+      >
+        {children}
+      </p>
+    );
+  }
+);
+
+Paragraph.displayName = "Paragraph";
+export default Paragraph;
